2fbd06f164ec74698c8716100b50177cd5e8c07a
silviapetrino <petrino.silvia@gmail.com>
2023-12-05 13:04:04 +0100

    [fix] page title

--- resources/views/admin/technologies/project-technologies.blade.php
+++ resources/views/admin/technologies/project-technologies.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="container">
-    <h1 class="text-white">Project List made with {{ $technology->name }} technology</h1>
+    <h1 class="text-white">Projects made with {{ $technology->name }} technology</h1>
 
 
 <table class="table table-dark">
